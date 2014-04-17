3716758ca8e648929be90f520d7203c4193d564e
Cody Jenkins <cody.jenkins@resmed.com>
2014-04-17 15:27:41 -0700

    Move metadata into separate files

--- js/main.js
+++ js/main.js
@@ -1,76 +1,8 @@
 (function (ResMed, ko) {
 
-  var DEVICE_SETTINGS_METADATA = [
-    {
-      enumName: 'PRESSURE',
-      displayName: 'Set pressure',
-      units: 'cmH2O',
-      values: { from: 4.0, to: 20.0, step: 0.2 },
-      defaultValue: 8.0
-    },
-    {
-      enumName: 'EPR_MODE',
-      displayName: 'EPR Mode',
-      values: [
-        { text: 'Off', value: 'OFF' },
-        { text: 'Ramp', value: 'RAMP_ONLY' },
-        { text: 'Fulltime', value: 'FULLTIME' }
-      ],
-      defaultValue: 'OFF'
-    },
-    {
-      enumName: 'EPR_LEVEL',
-      displayName: 'EPR Level',
-      values: [
-        { text: '1', value: 'ONE' },
-        { text: '2', value: 'TWO' },
-        { text: '3', value: 'THREE' }
-      ],
-      enable: [
-        { setting: 'EPR_MODE', notEqTo: 'OFF' }
-      ],
-      defaultValue: 0
-    },
-    {
-      enumName: 'EPAP_ASV',
-      displayName: 'EPAP',
-      units: 'cmH2O',
-      values: { from: 4.0, to: 15.0, step: 0.2 },
-      defaultValue: 8.0
-    },
-    {
-      enumName: 'MIN_PS',
-      displayName: 'Min PS',
-      units: 'cmH2O',
-      values: { from: 3.0, to: 6.0, step: 0.2 },
-      defaultValue: 4.0
-    },
-    {
-      enumName: 'MAX_PS',
-      displayName: 'Max PS',
-      units: 'cmH2O',
-      values: { from: 8.0, to: 16.0, step: 0.2 },
-      defaultValue: 12.0
-    }
-  ];
-
-  var DEVICE_MODES_METADATA = [
-    {
-      enumName: 'CPAP',
-      displayName: 'CPAP',
-      editableSettings: [ 'PRESSURE', 'EPR_MODE', 'EPR_LEVEL' ]
-    },
-    {
-      enumName: 'ASV',
-      displayName: 'ASV',
-      editableSettings: [ 'EPAP_ASV', 'MIN_PS', 'MAX_PS' ]
-    }
-  ];
-
-  /* Apply! */
   ko.applyBindings(new ResMed.ECO.Prescription.EditPrescriptionViewModel({
-    settingsMetadata: DEVICE_SETTINGS_METADATA,
-    modesMetadata: DEVICE_MODES_METADATA
+    settingsMetadata: window.DEVICE_SETTING_METADATA,
+    modesMetadata:    window.DEVICE_MODE_METADATA
   }));
 
 }(window.ResMed, window.ko));
---
+++ metadata/device-mode-metadata.js
@@ -0,0 +1,11 @@
+window.DEVICE_MODE_METADATA = [
+  {
+    "enumName": "CPAP",
+    "displayName": "CPAP",
+    "editableSettings": [ "PRESSURE", "EPR_MODE", "EPR_LEVEL" ]
+  }, {
+    "enumName": "ASV",
+    "displayName": "ASV",
+    "editableSettings": [ "EPAP_ASV", "MIN_PS", "MAX_PS" ]
+  }
+];
